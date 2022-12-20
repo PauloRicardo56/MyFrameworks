d197f07957453538c51638a2a9519ba8e54c1e95
Paulo Ricardo <praraujo.vieira@gmail.com>
2022-12-20 13:14:39 -0300

    First commit

---
+++ MyFrameworks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFrameworks
+//
+//  Created by Paulo Ricardo de Araujo Vieira on 20/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MyPod/MyPod/MyPod.h
@@ -0,0 +1,18 @@
+//
+//  MyPod.h
+//  MyPod
+//
+//  Created by Paulo Ricardo de Araujo Vieira on 20/12/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyPod.
+FOUNDATION_EXPORT double MyPodVersionNumber;
+
+//! Project version string for MyPod.
+FOUNDATION_EXPORT const unsigned char MyPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyPod/PublicHeader.h>
+
+
